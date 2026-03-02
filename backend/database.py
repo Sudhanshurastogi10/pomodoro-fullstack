5c22a00c6f3df8558f5347089918607326eabb10
Sudhanshurastogi10 <sudhanshurastogi2003@gmail.com>
2026-03-03 00:16:31 +0530

    Fix SQLite path for Render

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./pomodoro.db")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:////var/data/pomodoro.db"
+)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
